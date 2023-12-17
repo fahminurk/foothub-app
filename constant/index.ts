a96169cdd6e990fcfb8210741021eff8ad3ed98e
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-17 23:33:37 +0700

    feat: add folder constant

---
+++ constant/index.ts
@@ -0,0 +1,29 @@
+export const brands = [
+  {
+    title: "Adidas",
+    href: "/",
+  },
+  {
+    title: "Nike",
+    href: "/",
+  },
+  {
+    title: "Converse",
+    href: "/",
+  },
+];
+
+export const subcategory = [
+  {
+    title: "Casual",
+    href: "/",
+  },
+  {
+    title: "Running",
+    href: "/",
+  },
+  {
+    title: "Basketball",
+    href: "/",
+  },
+];
