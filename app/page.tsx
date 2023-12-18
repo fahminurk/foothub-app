322f9fff2935196b936b83563d2222b009528a92
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-18 22:49:08 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,47 @@
+"use client";
+import PromotionBanner from "@/components/PromotionBanner";
+import { homeText } from "@/constant";
+import { fontMono } from "@/lib/fonts";
+import { cn } from "@/lib/utils";
+import gsap from "gsap";
+
 export default function Home() {
+  const mouseEnter = (e: any, i: number) => {
+    gsap.to(e.target, {
+      top: "-2vw",
+      backgroundColor: homeText[i].color,
+      duration: 0.3,
+    });
+  };
+  const mouseLeave = (e: any, i: number) => {
+    gsap.to(e.target, {
+      top: "0",
+      backgroundColor: "white",
+      duration: 0.3,
+      delay: 0.1,
+    });
+  };
   return (
-    <main className="container relative">
-      <div className=" font-bold text-4xl">ssssssssss</div>
+    <main className="container flex flex-col gap-3 relative">
+      <div className="flex h-[90vh]  items-center">
+        <div className="relative w-full">
+          {homeText.map((item, i) => {
+            return (
+              <div
+                key={i}
+                className="border-t relative mb-[-6vw] md:mb-[-4vw] bg-white"
+                onMouseEnter={(e) => mouseEnter(e, i)}
+                onMouseLeave={(e) => mouseLeave(e, i)}
+              >
+                <p className="text-[13vw] md:text-[8vw] pl-2 uppercase pointer-events-none">
+                  {item.title}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <PromotionBanner />
     </main>
   );
 }
