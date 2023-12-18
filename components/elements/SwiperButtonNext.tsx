0210b255c4664725ad210f78702207e3fbd96e9c
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-18 22:47:35 +0700

    feat: add button swiper

---
+++ components/elements/SwiperButtonNext.tsx
@@ -0,0 +1,18 @@
+import { Button } from "../ui/button";
+import { useSwiper } from "swiper/react";
+import { PiCaretRightBold } from "react-icons/pi";
+
+const SwiperButtonNext = () => {
+  const swiper = useSwiper();
+  return (
+    <Button
+      className={`my-swiper-button-prev hidden border bg-white hover:bg-white/70 rounded-full w-10 h-10 absolute top-[45%] right-4 z-10 justify-center items-center md:flex`}
+      onClick={() => swiper.slideNext()}
+      size={"sm"}
+    >
+      <PiCaretRightBold className="fill-black dark:fill-white" size={20} />
+    </Button>
+  );
+};
+
+export default SwiperButtonNext;
---
+++ components/elements/SwiperButtonPrev.tsx
@@ -0,0 +1,18 @@
+import { Button } from "../ui/button";
+import { useSwiper } from "swiper/react";
+import { PiCaretLeftBold } from "react-icons/pi";
+
+const SwiperButtonPrev = () => {
+  const swiper = useSwiper();
+  return (
+    <Button
+      className={`my-swiper-button-prev hidden border bg-white hover:bg-white/70 rounded-full w-10 h-10 absolute top-[45%] left-4 z-10 justify-center items-center md:flex`}
+      onClick={() => swiper.slidePrev()}
+      size={"sm"}
+    >
+      <PiCaretLeftBold className="fill-black dark:fill-white" />
+    </Button>
+  );
+};
+
+export default SwiperButtonPrev;
