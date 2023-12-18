36bc5e1ac2a161baa0936c18e94120101bbabb1c
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-18 22:48:32 +0700

    update: add hometext variable

--- constant/index.ts
+++ constant/index.ts
@@ -27,3 +27,28 @@ export const subcategory = [
     href: "/",
   },
 ];
+
+export const homeText = [
+  {
+    title: "Your",
+
+    color: "#F06318",
+  },
+
+  {
+    title: "Fashionable",
+
+    color: "#DCF018",
+  },
+
+  {
+    title: "Footwear",
+
+    color: "#18F0E8",
+  },
+  {
+    title: "Starts here",
+
+    color: "#8C0CF0",
+  },
+];
