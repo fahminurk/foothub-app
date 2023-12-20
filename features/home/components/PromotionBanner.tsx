5be46f95e4180fe18401700e5b2bf2706a7e2aa3
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-20 10:56:14 +0700

    feat: add promotion banner

---
+++ features/home/components/PromotionBanner.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import SwiperButtonPrev from "@/components/elements/SwiperButtonPrev";
+import SwiperButtonNext from "@/components/elements/SwiperButtonNext";
+
+const PromotionBanner = () => {
+  return (
+    <Swiper
+      slidesPerView={1}
+      grabCursor={true}
+      loop={true}
+      autoplay={{
+        delay: 4000,
+        disableOnInteraction: false,
+      }}
+      pagination={true}
+      modules={[Autoplay, Navigation, Pagination]}
+      className={`mySwiper w-full relative`}
+    >
+      <SwiperButtonPrev />
+      <SwiperSlide className="bg-neutral-800 h-full">
+        <img
+          src={"/images/banner1.jpg"}
+          alt="foothub"
+          className="w-full h-full object-cover"
+        />
+      </SwiperSlide>
+      <SwiperSlide className="bg-neutral-800 h-full">
+        <img
+          src={"/images/banner2.jpg"}
+          alt="foothub"
+          className="w-full h-full object-cover"
+        />
+      </SwiperSlide>
+      <SwiperButtonNext />
+    </Swiper>
+  );
+};
+
+export default PromotionBanner;
