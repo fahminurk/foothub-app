7162ed8f1bf2b7b0b59e0b5d48da4ea91408499a
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-22 23:59:56 +0700

    update: styling

--- components/AdidasBanner.tsx
+++ components/AdidasBanner.tsx
@@ -9,7 +9,9 @@ const AdidasBanner = () => {
         className="h-80 md:h-auto object-cover"
       />
       <div className="absolute text-white bottom-0 gap-2 px-4 flex flex-col w-full h-full justify-center">
-        <p className="text-2xl md:text-9xl font-bold">MAKE YOUR SAMBA COUNT</p>
+        <p className="text-2xl md:text-4xl lg:text-5xl font-bold">
+          MAKE YOUR SAMBA COUNT
+        </p>
         <p className="text-sm md:text-xl">{`Here's my Samba retreat. What's yours?`}</p>
         <div>
           <Button className="">Shop Now</Button>
