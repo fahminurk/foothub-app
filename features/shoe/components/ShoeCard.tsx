154ba6d7b247c8710446ca109962b5b9a0a42a8e
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-24 00:01:40 +0700

    update: change src img

--- features/shoe/components/ShoeCard.tsx
+++ features/shoe/components/ShoeCard.tsx
@@ -23,14 +23,12 @@ type ShoeCardProps = {
 };
 
 const ShoeCard: React.FC<ShoeCardProps> = ({ item }) => {
-  console.log(item);
-
   return (
     <Card>
       <CardContent className="p-0">
         <AspectRatio>
           <Image
-            src={`http://localhost:8000/${item.shoeImage[0].imgUrl}`}
+            src={item.shoeImage[0].imgUrl}
             alt="img"
             fill
             className="w-full object-cover"
@@ -39,7 +37,9 @@ const ShoeCard: React.FC<ShoeCardProps> = ({ item }) => {
       </CardContent>
       <CardFooter className="p-0">
         <div className="p-1 flex flex-col w-full gap-1">
-          <p className="font-semibold truncate">{item.name.toUpperCase()}</p>
+          <p className="font-semibold truncate overflow-ellipsis">
+            {item.name.toUpperCase()}
+          </p>
           <Separator />
           <p className="text-sm">{formatToIDR(item.price)}</p>
         </div>
