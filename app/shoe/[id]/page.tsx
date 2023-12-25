0e9d5cfc74b554dadbed18cf4d62c2c4eda3631c
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-25 23:05:59 +0700

    update: clean code

--- app/shoe/[id]/page.tsx
+++ app/shoe/[id]/page.tsx
@@ -1,18 +1,11 @@
 "use client";
 import { useShoeDetailQuery } from "@/actions/useShoe";
-import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
 import SidebarShoeDetails from "@/features/shoe/components/SidebarShoeDetails";
-import { formatToIDR } from "@/lib/utils";
-import { useAuthStore } from "@/store/authStore";
 import React, { useState } from "react";
 
 const Page = ({ params }: { params: { id: string } }) => {
-  const user = useAuthStore((state) => state.user);
   const { id } = params;
   const { data } = useShoeDetailQuery(id);
-  const [size, setSize] = useState<string | null>(null);
-  const [stock, setStock] = useState<number | null>(null);
 
   if (!data) return <div>loading</div>;
   return (
