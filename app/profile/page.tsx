8d69ead3b94a68c2d798bf8e97c8d221740f7da4
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-27 20:48:57 +0700

    feat: add profile page

---
+++ app/profile/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import ProfileForm from "@/features/profile/ProfileForm";
+
+const Page = () => {
+  return (
+    <div className="container my-5">
+      <Tabs defaultValue="account" className="w-full">
+        <TabsList className="grid w-full grid-cols-3">
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="address">Address Book</TabsTrigger>
+          <TabsTrigger value="order">My Orders</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">
+          <Card className="lg:p-8">
+            <CardHeader>
+              <CardTitle>YOUR DETAILS</CardTitle>
+              <CardDescription>
+                Make changes to your account here. Click save when you&apos;re
+                done.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <ProfileForm />
+            </CardContent>
+          </Card>
+        </TabsContent>
+        <TabsContent value="address">
+          <Card>
+            <CardHeader>
+              <CardTitle>Password</CardTitle>
+              <CardDescription>
+                Change your password here. After saving, you&apos;ll be logged
+                out.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="current">Current password</Label>
+                <Input id="current" type="password" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="new">New password</Label>
+                <Input id="new" type="password" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Save password</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
+
+export default Page;
