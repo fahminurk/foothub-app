e561b03687f5c9409c42a5771540cf1694e851f6
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-27 23:15:17 +0700

    update: refactor profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -8,9 +7,8 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import AddressForm from "@/features/profile/AddressForm";
 import ProfileForm from "@/features/profile/ProfileForm";
 
 const Page = () => {
@@ -37,27 +35,12 @@ const Page = () => {
           </Card>
         </TabsContent>
         <TabsContent value="address">
-          <Card>
-            <CardHeader>
-              <CardTitle>Password</CardTitle>
-              <CardDescription>
-                Change your password here. After saving, you&apos;ll be logged
-                out.
-              </CardDescription>
+          <Card className="lg:p-8">
+            <CardHeader className="flex flex-row justify-between items-center">
+              <CardTitle>ADDRESS BOOK</CardTitle>
+              <AddressForm />
             </CardHeader>
-            <CardContent className="space-y-2">
-              <div className="space-y-1">
-                <Label htmlFor="current">Current password</Label>
-                <Input id="current" type="password" />
-              </div>
-              <div className="space-y-1">
-                <Label htmlFor="new">New password</Label>
-                <Input id="new" type="password" />
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button>Save password</Button>
-            </CardFooter>
+            <CardContent className="space-y-2">{/*  */}</CardContent>
           </Card>
         </TabsContent>
       </Tabs>
