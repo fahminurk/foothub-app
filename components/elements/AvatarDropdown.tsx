8ea43976d4b30c03c37a28dcbd79c94aca53e397
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-28 22:57:21 +0700

    Update AvatarDropdown.tsx

--- components/elements/AvatarDropdown.tsx
+++ components/elements/AvatarDropdown.tsx
@@ -14,31 +14,29 @@ export const AvatarDropdown = () => {
   const { user, onLogout } = useAuthStore();
 
   return (
-    <>
-      <DropdownMenu>
-        <DropdownMenuTrigger className="self-start">
-          <Avatar className="h-10 w-10">
-            <AvatarFallback>{user?.name.charAt(0)}</AvatarFallback>
-            <AvatarImage src={user?.avatarUrl} />
-          </Avatar>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end" sideOffset={8}>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem asChild>
-            <Link href="/dashboard">Dashboard Creator</Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem asChild>
-            <Link href="/profile">Profile</Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem
-            onClick={onLogout}
-            className="font-semibold text-red-500"
-          >
-            Logout
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </>
+    <DropdownMenu>
+      <DropdownMenuTrigger className="self-start">
+        <Avatar className="h-10 w-10">
+          <AvatarFallback>{user?.name.charAt(0)}</AvatarFallback>
+          <AvatarImage src={user?.avatarUrl} />
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" sideOffset={8}>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href="/dashboard">Dashboard Creator</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <Link href="/profile">Profile</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={onLogout}
+          className="font-semibold text-red-500"
+        >
+          Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
