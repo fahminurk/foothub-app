580bced1c7ce6f3e52aabf5df91db08fe7ef86b8
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-28 22:58:23 +0700

    Update axios.ts

--- lib/axios.ts
+++ lib/axios.ts
@@ -6,7 +6,7 @@ const api = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? process.env.NEXT_PUBLIC_API_URL_DEV
-      : process.env.NEXT_PUBLIC_API_URL_DEV,
+      : process.env.NEXT_PUBLIC_API_URL_PROD,
 });
 
 api.interceptors.request.use((config) => {
