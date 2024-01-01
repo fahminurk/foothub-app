9753c33c8c5dd42c3596f68fdb9afb3bd8e4ec61
Fahmi Nurkamil <fahminurk31@gmail.com>
2024-01-01 18:01:46 +0700

    update: base api

--- lib/axios.ts
+++ lib/axios.ts
@@ -3,10 +3,10 @@ import { useAuthStore } from "@/store/authStore";
 import { toast } from "sonner";
 
 const api = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "development"
-      ? process.env.NEXT_PUBLIC_API_URL_DEV
-      : process.env.NEXT_PUBLIC_API_URL_PROD,
+  baseURL: "https://foothubapi.fahminurkamil.tech",
+  // process.env.NODE_ENV === "development"
+  //   ? process.env.NEXT_PUBLIC_API_URL_DEV
+  //   : process.env.NEXT_PUBLIC_API_URL_PROD,
 });
 
 api.interceptors.request.use((config) => {
